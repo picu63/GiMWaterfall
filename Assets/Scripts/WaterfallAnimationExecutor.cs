a973e3813234e4e06348f5883c442d80f02327c9
picu63 <48946087+picu63@users.noreply.github.com>
2021-02-07 14:52:55 +0100

    first commit

---
+++ Assets/Scripts/WaterfallAnimationExecutor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WaterfallAnimationExecutor : MonoBehaviour
+{
+    public KeyCode KeyToAnimateStart;
+    private ParticleSystem _particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var waterfall 
+            = GameObject.FindGameObjectWithTag("Waterfall");
+        _particleSystem
+            = waterfall.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyToAnimateStart))
+            return;
+
+        if (_particleSystem.isPlaying)
+            _particleSystem.Stop();
+        else
+            _particleSystem.Play();
+    }
+}
